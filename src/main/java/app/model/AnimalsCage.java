bac39f94e6f9955bed4efd63e22b387437ec7b86
Evgeniy Strigo <evgstrigo@mail.ru>
2022-02-19 23:56:43 +0300

    решено

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,44 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope(value = "prototype")
 public class AnimalsCage {
 
+
     @Autowired
+    @Qualifier(value = "dog")
     private Animal animal;
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,16 +1,13 @@
 package app.model;
 
-
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Scope(value = "prototype")
-public class Timer {
+public class Dog extends Animal {
 
-    private Long nanoTime = System.nanoTime();
-
-    public Long getTime() {
-        return nanoTime;
-    }
+    @Override
+    public String toString() { return "Im a Dog"; }
 }
