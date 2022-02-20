f497ddbd8dec86bad415c68161266c2a8cd2f4a6
Evgeniy Strigo <evgstrigo@mail.ru>
2022-02-20 22:36:57 +0300

    решил 20.02 вечер

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -4,6 +4,7 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
@@ -11,19 +12,31 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public User getUserByCarModelQuery(String model, int series) {
+        return (User) sessionFactory
+                .getCurrentSession()
+                .getNamedQuery("getUserByCarModelAndSeries")
+                .setParameter("carModel", model)
+                .setParameter("carSeries", series)
+                .getSingleResult();
+    }
+
 
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -6,5 +6,9 @@ import java.util.List;
 
 public interface UserService {
     void add(User user);
+
     List<User> listUsers();
+
+    User getUserByCarModelQuery(String model, int series);
+
 }
